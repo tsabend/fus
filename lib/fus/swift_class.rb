90e305792fa08276baed1aa764f85e43d5936d21
tsabend <tsabend@gmail.com>
2017-01-20 07:42:07 -0500

    Add docs

--- lib/fus/swift_class.rb
+++ lib/fus/swift_class.rb
@@ -1,26 +1,32 @@
 module Fus
+  # A thin wrapper around the name of a Swift class
   class SwiftClass
     attr_reader :name
     def initialize(name)
       @name = name
     end
     
+    # Is this class a test class
     def spec?
       name.match(/Spec|Tests|Test/)
     end
     
+    # Does this class name match this path
     def matches_classname?(path)
       path.match(/#{name}\b/)
     end
     
+    # Is this class used in this XML
     def used_in_xml?(xml)
       xml.include?("customClass=\"#{name}\"")
     end
     
+    # Is this class used in this Obj-C text
     def used_in_obj_c?(text)
       text.match(/(^|[^@])#{name}/)
     end
     
+    # Is this class used in this Swift text
     def used_in_swift?(text)
       text.match(/(\b#{name}.?[.:(])|([:].?#{name})|((typealias|associatedType)\s+.*=.+#{name})/)
     end
